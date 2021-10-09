6b70624e722f65c84c72e288ee85c9a18698a05d
Eli Barzilay <eli@barzilay.org>
2021-10-09 06:44:12 -0400

    .

--- .github/workflows/shared.sh
+++ .github/workflows/shared.sh
@@ -1,9 +1,15 @@
-gh auth login --with-token <<<"$GH_TOKEN"
+LOGGED_IN=0
+login() {
+  if ((LOGGED_IN)); then return; fi
+  echo "Logging into GH"
+  gh auth login --with-token <<<"$GH_TOKEN"
+}
 
-H=(-H "Accept:application/vnd.github.inertia-preview+json")
-api() { gh api "${H[@]}" "$@"; }
+HDRS=()
+# HDRS=(-H "Accept:application/vnd.github.inertia-preview+json")
+api() { gh api "${HDRS[@]}" "$@"; }
 
-proj="$(api "repos/:owner/:repo/projects" | jq -r '.[0].id')"
-col="$(api "projects/$proj/columns" | jq -r '.[] | @text "\(.name):\(.id)"' | \
-         grep -Ei "^To ?do:")"
-col="${col##*:}"
+# proj="$(api "repos/:owner/:repo/projects" | jq -r '.[0].id')"
+# col="$(api "projects/$proj/columns" | jq -r '.[] | @text "\(.name):\(.id)"' | \
+#          grep -Ei "^To ?do:")"
+# col="${col##*:}"
