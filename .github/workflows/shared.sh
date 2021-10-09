a9704f7e1909812c376b987482ac4ead059b435b
Eli Barzilay <eli@barzilay.org>
2021-10-09 18:13:09 -0400

    .

--- .github/workflows/shared.sh
+++ .github/workflows/shared.sh
@@ -14,12 +14,24 @@ ctx() {
   jq -r "$*" <<<"$GH_CTX"
 }
 
+LF=$'\n'
+# convert an id to a name or the other way
+id_name() { # table id-or-name
+  local rx="$LF([^$LF]*):$2:([^$LF]*)$LF"
+  if [[ "$1" =~ $rx ]]; then
+    echo "${BASH_REMATCH[1]#:}${BASH_REMATCH[2]%:}"
+  else
+    echo "error: could not find $2 in {$1}" 1>&2; exit 1
+  fi
+}
+
 PROJs=""
 get_PROJs() {
   if [[ -n "$PROJs" ]]; then return; fi
   login
   PROJs="$(api "repos/:owner/:repo/projects" | \
-           jq -r '.[] | @text "\(.name):\(.id)"')"
+           jq -r '.[] | @text ":\(.name):\(.id):"')"
+  PROJs="$LF$PROJs$LF"
 }
 
 # col="$(api "projects/$proj/columns" | jq -r '.[] | @text "\(.name):\(.id)"' | \
