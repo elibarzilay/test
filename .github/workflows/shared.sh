88e5ee3abdf4c3652602ec59fb33f1f4f0f29644
Eli Barzilay <eli@barzilay.org>
2021-10-09 06:52:44 -0400

    .

--- .github/workflows/shared.sh
+++ .github/workflows/shared.sh
@@ -10,7 +10,15 @@ HDRS=()
 # HDRS=(-H "Accept:application/vnd.github.inertia-preview+json")
 api() { gh api "${HDRS[@]}" "$@"; }
 
-# proj="$(api "repos/:owner/:repo/projects" | jq -r '.[0].id')"
+PROJ_ID=""
+proj_id() {
+  if [[ -z "$PROJ_ID" ]]; then
+    login
+    PROJ_ID="$(api "repos/:owner/:repo/projects" | jq -r '.[0]')"
+  fi
+  echo "$PROJ_ID"
+}
+
 # col="$(api "projects/$proj/columns" | jq -r '.[] | @text "\(.name):\(.id)"' | \
 #          grep -Ei "^To ?do:")"
 # col="${col##*:}"
